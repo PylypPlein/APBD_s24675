def97ea3840876aa4579cf3e5005c8ef1cbbd042
s24675 <s24675@pjwstk.edu.pl>
2024-03-15 17:45:45 +0100

    modyfikacja nazwy tabeli na numbersCollection

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int[] numbers = { 3, 4, 5, 6, 7, 1, 2, 3 };
-Console.WriteLine(Calculate(numbers));
+int[] numbersCollection = { 3, 4, 5, 6, 7, 1, 2, 3 };
+Console.WriteLine(Calculate(numbersCollection));
 
 static double Calculate(int[] numbers)
 {
