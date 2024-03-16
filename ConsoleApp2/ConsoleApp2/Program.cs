c0a3411ab295d89d5d658652fead6e3649ca75f7
s24675 <s24675@pjwstk.edu.pl>
2024-03-16 23:14:45 +0100

    zmiana nazwy metody na CalculateAVG

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 int[] numbers = { 3, 4, 5, 6, 7, 1, 2, 3 };
-Console.WriteLine(Calculate(numbers));
+Console.WriteLine(CalculateAVG(numbers));
 
-static double Calculate(int[] numbers)
+static double CalculateAVG(int[] numbers)
 {
     return numbers.Average();
 }
