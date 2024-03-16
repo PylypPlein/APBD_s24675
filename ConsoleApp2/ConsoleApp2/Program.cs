7fc3ed17aea9f143f52e32b2594f83165ba48906
s24675 <s24675@pjwstk.edu.pl>
2024-03-16 23:17:00 +0100

    Zmiana nazwy funkji na CalculateAvg

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 int[] numbersCollection = { 3, 4, 5, 6, 7, 1, 2, 3 };
-Console.WriteLine(Calculate(numbersCollection));
+Console.WriteLine(CalculateAvg(numbersCollection));
 
-static double Calculate(int[] numbers)
+static double CalculateAvg(int[] numbers)
 {
     return numbers.Average();
 }
